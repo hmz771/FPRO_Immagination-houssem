0d5cbfe5890fba1515f74690dd110c00f0f06086
hamza <hamza@hamza-VAIO.Home>
2014-04-22 20:26:36 +0200

    hhhh

---
+++ FProComming_16_04/src/Controllers/Client_CTL.java
@@ -0,0 +1,39 @@
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+
+public class Client_CTL implements Initializable 
+{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@FXML
+	private Label label1;
+	@FXML
+	private Button ENREG_BUTT;
+	String ggg="";
+	public void Enreg_Action()
+	{
+		label1.setText("hamza");
+		
+		
+	}
+	
+	
+	
+
+	
+
+}
